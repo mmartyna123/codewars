6b48fff554abf667a2a591f9a7b8dc0c43dadffa
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-09-15 22:10:14 +0200

    new kata

---
+++ 8kyu/Square(n) Sum.py
@@ -0,0 +1,27 @@
+# Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+# For example, for [1, 2, 2] it should return 9 because 
+# 1
+# 2
+# +
+# 2
+# 2
+# +
+# 2
+# 2
+# =
+# 9
+# 1 
+# 2
+#  +2 
+# 2
+#  +2 
+# 2
+#  =9.
+
+# my solution:
+def square_sum(numbers):
+    S=0
+    for i in numbers:
+        S += i*i
+    return S

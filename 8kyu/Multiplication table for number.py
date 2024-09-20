7242f3e329a8059dc15b7aac1c5a22898260f2f1
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-09-20 23:30:03 +0200

    new kata

--- 8kyu/Multiplication table for number.py
+++ 8kyu/Multiplication table for number.py
@@ -1,9 +1,11 @@
 # Your goal is to return multiplication table for number that is always an integer from 1 to 10.
 
-# my solution:
-def multi_table(number):
+# my solutions:
+def multi_table0(number):
     result = []
     for i in range(1, 11):
         result.append(f'{i} * {number} = {i*number}')
     return '\n'.join(result)
     
+def multi_table1(number):
+    return '\n'.join(f'{i} * {number} = {i*number}' for i in range(1,11))

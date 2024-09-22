8b217c7d3a407afb0720d9591327bb2d64df137a
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-09-22 22:44:38 +0200

    new kata

--- 8kyu/Merge two sorted arrays into one.py
+++ 8kyu/Merge two sorted arrays into one.py
@@ -3,3 +3,7 @@
 # You don't need to worry about validation, since arr1 and arr2 must be arrays with 0 or more Integers. If both arr1 and arr2 are empty, then just return an empty array.
 
 # Note: arr1 and arr2 may be sorted in different orders. Also arr1 and arr2 may have same integers. Remove duplicated in the returned result.
+
+# my solution:
+def merge_arrays(arr1, arr2):
+    return sorted(set(arr1) | set(arr2))

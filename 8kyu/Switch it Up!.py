ddf19cc25ac5c57b1897a658da068d5970c08f61
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-09-22 22:36:28 +0200

    new solution to kata

--- 8kyu/Switch it Up!.py
+++ 8kyu/Switch it Up!.py
@@ -4,7 +4,12 @@
 
 # Output :: "One".
 
-# my solution:
-def switch_it_up(number):
+# my solutions:
+def switch_it_up0(number):
     numbers = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+    return numbers[number]
+
+def switch_it_up1(number):
+    numbers = { 0:'Zero', 1:'One', 2:'Two', 3:'Three', 4:'Four', 5:'Five', 
+               6:'Six', 7:'Seven', 8:'Eight', 9:'Nine'}
     return numbers[number]

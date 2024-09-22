6e8cc52795d6f719a5b516985c32f757d6cf030f
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-09-22 22:32:32 +0200

    new kata

--- 8kyu/Switch it Up!.py
+++ 8kyu/Switch it Up!.py
@@ -5,3 +5,6 @@
 # Output :: "One".
 
 # my solution:
+def switch_it_up(number):
+    numbers = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+    return numbers[number]

1224d257d3877c07e00bc73a619d44e15680b2f0
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-09-27 00:25:12 +0200

    New solution to the kata

--- 7kyu/Sum of a sequence.py
+++ 7kyu/Sum of a sequence.py
@@ -11,6 +11,13 @@
 # 1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
 # 1,5,3  --> 5 (1 + 4)
 
-# my solution:
-def sequence_sum(begin_number, end_number, step):
+# my solutions:
+def sequence_sum0(begin_number, end_number, step):
     return sum(range(begin_number, end_number+1, step)) if begin_number <= end_number else 0
+
+def sequence_sum1(begin_number, end_number, step):
+    if begin_number > end_number and step > 0 or begin_number < end_number and step < 0:
+        return 0
+    n = (end_number - begin_number) // step + 1
+    last_number = begin_number + (n-1)*step
+    return n*(begin_number + last_number) //2

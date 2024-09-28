9a3aceab92e5206eccf8e5c43f9a959daa341eda
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-09-28 18:44:33 +0200

    New solution to the kata

--- 7kyu/Factorial.py
+++ 7kyu/Factorial.py
@@ -2,11 +2,17 @@
 
 # Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C).
 
-# my solution:
-def factorial(n):
+# my solutions:
+def factorial0(n):
     result = 1
     if n < 0 or n > 12:
         raise ValueError
     for i in range(1, n+1):
         result *= i
     return result
+
+import math
+def factorial1(n):
+    if n < 0 or n > 12:
+        raise ValueError
+    return math.factorial(n)

8c835eb78db65a7807ee308b6eb8ec86cfaf3582
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-09-28 23:22:35 +0200

    New solution to the kata

--- 8kyu/Removing Elements.py
+++ 8kyu/Removing Elements.py
@@ -3,11 +3,13 @@
 # Example:
 # ["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
 
-# my solution:
-def remove_every_other(my_list):
+# my solutions:
+def remove_every_other0(my_list):
     new_list=[]
     for i in range(len(my_list)):
         if i % 2 == 0:
             new_list.append(my_list[i])
     return new_list
         
+def remove_every_other1(my_list):
+    return my_list[::2]

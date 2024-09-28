2ea6ad2ddbc6794ec62d0de5576b2f0b3cb999a2
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-09-28 19:58:54 +0200

    New solution to the kata

--- 8kyu/altERnaTIng cAsE  ALTerNAtiNG CaSe.py
+++ 8kyu/altERnaTIng cAsE  ALTerNAtiNG CaSe.py
@@ -1,7 +1,7 @@
 # Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase.
 
 # my solution:
-def to_alternating_case(string):
+def to_alternating_case0(string):
     result= ""
     for character in string:
         if character.islower():
@@ -9,3 +9,7 @@ def to_alternating_case(string):
         else:
             result+=character.lower()
     return result
+
+def to_alternating_case1(string):
+    return string.swapcase()
+

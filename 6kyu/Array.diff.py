bb5fed3a4ac1d02f27902c82994151b44e3b72a8
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-09-30 21:33:04 +0200

    New solution to the kata

--- 6kyu/Array.diff.py
+++ 6kyu/Array.diff.py
@@ -2,10 +2,13 @@
 
 # It should remove all values from list a, which are present in list b keeping their order.
 
-# my solution:
-def array_diff(a, b):
+# my solutions:
+def array_diff0(a, b):
     res=[]
     for element in a:
         if element not in b:
             res.append(element)
     return res
+
+def array_diff1(a, b):
+    return [element for element in a if element not in b]

a66279d0d56aa295c189fe7dbdf6dcdfbbe85015
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-09-30 20:38:00 +0200

    New kata

---
+++ 5kyu/Integer to English.py
@@ -0,0 +1,16 @@
+# For a given positive integer convert it into its English representation. All words are lower case and are separated with one space. No trailing spaces are allowed.
+
+# To keep it simple, hyphens and the writing of the word 'and' both aren't enforced. (But if you are looking for some extra challenge, such an output will pass the tests.)
+
+# Large number reference: http://en.wikipedia.org/wiki/Names_of_large_numbers (U.S., Canada and modern British)
+
+# Input range: 1 -> 10**26
+# Examples:
+
+# int_to_english(1) == 'one'
+
+# int_to_english(10) == 'ten'
+
+# int_to_english(25161045656) == 'twenty five billion one hundred sixty one million forty five thousand six hundred fifty six'
+
+# my solution:

05213c6b19e0903ca9bf64e17abb7f04f317e0ef
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-10-01 18:32:41 +0200

    New kata

---
+++ 5kyu/k-divide.py
@@ -0,0 +1,44 @@
+# You are given a positive integer N and an integer K > 1.
+
+# For each integer from 1 to N, repeat the following operation:
+
+# Divide the integer by K
+# If the number is divisible, divide by K, and repeat
+# Otherwise, end the operation.
+# Let f(n) be the integer after the operation. For example, if n = 12 and K = 3, then f(12) = 4(12→4).
+
+# Find the sum of all f(n) from 1 to N.
+
+# Input
+# You be given:
+# N K
+# Constraints
+# The test cases are in the range
+
+# 1≤N≤10**18
+# 2≤K≤10**12
+
+# Examples
+# Let's try testing N=10 and K=4.
+
+# f(1)=1
+# f(2)=2
+# f(3)=3
+# f(4)=1
+# f(5)=5
+# f(6)=6
+# f(7)=7
+# f(8)=2
+# f(9)=9
+# f(10)=10
+# So, 1+2+3+1+5+6+7+2+9+10=46
+
+# my solution:
+def calculate_sum(N, K):
+    sum = 0      
+    for number in range(1,N+1):
+        while number % K ==0:
+            quotient, remainder = divmod(number, K)
+            number = quotient
+        sum += number     
+    return sum

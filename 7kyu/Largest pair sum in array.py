e8b18e641fa8d07c9ca459364808d5c1bfdf837e
mmartyna123 <mmartyna.stasiak@gmail.com>
2024-10-02 23:35:39 +0200

    new solution to the kata

--- 7kyu/Largest pair sum in array.py
+++ 7kyu/Largest pair sum in array.py
@@ -5,7 +5,17 @@
 # [10, 14, 2, 23, 19] -->  42 (= 23 + 19)
 # [99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
 
-# my solution:
-def largest_pair_sum(numbers): 
+# my solutions:
+def largest_pair_sum0(numbers): 
     numbers = sorted(numbers)
     return numbers[-1]+numbers[-2]
+
+def largest_pair_sum1(numbers): 
+    largest = second_largest = float('-inf')
+    for number in numbers:
+        if number > largest:
+            second_largest = largest
+            largest = number
+        elif number > second_largest:
+            second_largest = number
+    return largest+second_largest
